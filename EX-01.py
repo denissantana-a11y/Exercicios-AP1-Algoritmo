c1d5166e7445e470c73d34b381747a0d74916346
Denis Santana <denissantana@aluno.impacta.edu.br>
2026-09-11 19:57:09 -0300

    Exercicio1

---
+++ EX-01.py
@@ -0,0 +1,22 @@
+cidade = "São Paulo"
+print(cidade)
+#São Paulo
+
+5 + 2 * 3
+
+saudacao = "Olá, Mundo!"
+print(saudacao)
+#Olá, Mundo!
+
+calculo = 5 + 2 * 3
+#11
+print(calculo)
+
+expressao_texto = "5 + 2 * 3"
+print(expressao_texto)
+
+resultado = expressao_texto + " = " + str(calculo)
+print(resultado)
+
+resultado1 = f"{expressao_texto} = {calculo}"
+print(resultado1)
